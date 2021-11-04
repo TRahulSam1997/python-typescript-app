c3a4aba7829cb645963372739eed7ddc90bc30a3
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-11-04 21:11:26 +1100

    create react app for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todoItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    description: "Buy ingredients to prepare dinner",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for the upcoming test",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Sammy's books",
+    description: "Go to library to return Sammy's books",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write article on how to use Django with React",
+    completed: false,
+  },
+];
+
+interface Props {
+  todoItems: string[]
+}
+
+class App extends React.Component {
+  constructor(props:Props) {
+    super(props);
+    this.state = {
+      todoItems: todoItems,
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>Hello World!</div>
+      </div>
+    );
+  }
 }
 
 export default App;
