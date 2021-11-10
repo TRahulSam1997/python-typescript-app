2afef6fc221508ec261915e1785dc412eec82adb
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-11-10 22:00:16 +1100

    prop types added

---
+++ frontend/src/components/Modal.ts
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+
+// export default class CustomModal extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//           activeItem: this.props.activeItem,
+//         };
+//     }
+// }
